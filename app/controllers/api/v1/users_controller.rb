24f2bfc36a390b5e0193f7d71a9cb54a77c0ad4c
stubrew24 <sewelly@gmail.com>
2019-04-26 11:49:41 +0100

    signup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,6 +10,16 @@ class Api::V1::UsersController < ApplicationController
     render json: @user
   end
 
+  def create
+    @newuser = User.new(first_name: signup_params[:first_name],last_name: signup_params[:last_name],email: signup_params[:email],dob: signup_params[:dob],password: signup_params[:password])
+    if @newuser.save
+      token = encode_token(@newuser.id)
+      render json: {user: @newuser, token: token}
+    else
+      render json: {error: @newuser.errors.full_messages, status: 400}
+    end
+  end
+
   def signin 
     @user = User.find_by(email: signin_params[:email])
     if (@user && @user.password_digest === signin_params[:password])
@@ -20,6 +30,10 @@ class Api::V1::UsersController < ApplicationController
   end
 
   private
+
+  def signup_params
+    params.permit(:first_name, :last_name, :email, :password, :dob)
+  end
 
   def signin_params
     params.permit(:email, :password)
