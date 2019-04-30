321bd349080a3d66f15e2e58c46b5392feec193d
stubrew24 <sewelly@gmail.com>
2019-04-30 22:58:53 +0100

    Orders Page

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,3 +1,17 @@
 class OrderSerializer < ActiveModel::Serializer
-    attributes :id, :total, :delivery, :status, :user_id, :created_at, :products
+    attributes :id, :total, :delivery, :status, :created_at, :products, :user
+    def products
+        self.object.products.map do |product|
+            qty = self.object.order_products.find_by(product_id: product.id).qty
+            {
+                id: product.id,
+                name: product.name,
+                price: product.price,
+                image_url: product.image_url,
+                brewery_id: product.brewery.id,
+                brewery_name: product.brewery.name,
+                qty: qty
+            }
+        end
+    end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-	attributes :id, :first_name, :last_name, :email, :dob, :tel, :profile_img, :address_line_1, :address_line_2, :city, :postcode
+	attributes :id, :first_name, :last_name, :email, :dob, :tel, :profile_img, :address_line_1, :address_line_2, :city, :postcode, :orders
 end
